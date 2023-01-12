a073753bae2f943db1f9b557d02f25aead2fb644
Ahmed Samer <ahmedsam3r@gmail.com>
2023-01-12 10:28:50 +0200

    increased cohesion and reduced coupling.

--- 1-cohesion-coupling/README.md
+++ 1-cohesion-coupling/README.md
@@ -1,10 +1,26 @@
-# Code Analaysis
-<br>
-This method register_vehicle() is an example of (A)weak cohesion and (B)high coupling.<br>
-    (A)<br>
-        *Weak cohesion as it does a lot of different things (6) with no clear single responsibility.<br>
-        *It's hard to add a new car brand, as we have to add new if condition with the car brand and we will add it too in checking if electric or not
-        Comment: This one could be easily solved if we get the car from DB table, we won't need all this hassle. <br>
-    (B)<br>
-        *High coupling as it directly depends on the implementation details of VechileRegistry class.<br>
-        If anything changed in VechileRegistery class, I have to change register_vechile method in CarApplication class.<br>
+# Problem 
+* There's a direct coupling of brand name and catalogue price (bad)
+* We've a brand information, but also we've vechile instance information such as ID and lisence plate.
+* register_vehicle method has to know the implementation details of generate_id() and generate_lisence()
+
+* Printing info is now two things; printing vehicle and model info
+
+
+# Solution
+
+* Look at the information stored & where it's accessed from.
+
+* Start to group the code around the information, that will lead to lower coupling as it's more closer to the information it uses. 
+* The methods will have higher cohesion as they can only do one thing with that particular information.
+
+* As Craig Larman in his book (Applying UML and Patterns: An Introduction to Object-Oriented Analysis and Design and Iterative Development) where the information of your application is centred. You should look for information expert he stated.
+
+* We've to separate the brand info and vehicle info from each other 
+
+* Seprated print in two different classes where it's more closer to the info they're printing to reduce coupling and increse cohesion
+
+* Adding new models just require one change in one place
+
+* The code now is more maintainable and extendable
+
+* The changes 
--- 1-cohesion-coupling/cohesion-coupling.py
+++ 1-cohesion-coupling/cohesion-coupling.py
@@ -1,8 +1,63 @@
 import string
 import random
 
+#Same meaning as CarInfo or BrandInfo or Vechile Info
+class ModelInfo:
+    name: str
+    price: int
+    is_electric: bool
+
+    def __init__(self, name, price, is_electric) -> None:
+        self.name = name
+        self.price = price
+        self.is_electric = is_electric
+    
+    def compute_tax(self) -> int:
+        #(5) compute the tax percentage, default is 0.05 except for electric cars it goes down in the tax %
+        tax_percentage = 0.05
+        if self.is_electric:
+            tax_percentage = 0.02
+
+        return tax_percentage
+
+    def calculate_tax(self) -> float:
+        return self.compute_tax() * self.price
+
+    def print(self) -> None:
+        print("Model name: ",self.name)
+        print("Price: ", self.price)
+        print("Payable Tax: ",self.calculate_tax())
+
+
+
+class Vehicle:
+    id: str
+    lisence_plate: str
+    vehicle_info: ModelInfo
+
+    def __init__(self, id, lisence_plate, vehicle_info) -> None:
+        self.id = id
+        self.lisence_plate = lisence_plate
+        self.vehicle_info = vehicle_info
+
+    def print(self) -> None:
+        print("ID: ",self.id)
+        print("Lisence plate: ",self.lisence_plate)
+        self.vehicle_info.print()
+
+
 
 class VehicleRegistry:
+    model_info = {}
+
+    def add_model_info(self, model_name, price, is_electric):
+        self.model_info[model_name] = ModelInfo(model_name, price, is_electric)
+    
+    def __init__(self) -> None:
+        #TODO replace it with info from DB car table
+        self.add_model_info("VolksWagen ID3", 1000, True)
+        self.add_model_info("Tesla Model 3", 10000, True)
+        self.add_model_info("BMW 5", 100000, False)
 
     def generate_id(self, length):
         return ''.join(random.choices(string.ascii_uppercase, k=length))
@@ -10,42 +65,29 @@ class VehicleRegistry:
     def generate_lisence_plate(self, id):
         return f"{id}-{''.join(random.choices(string.digits, k=3))}"
 
-class CarApplication:
-    def register_vehicle(self, brand_name: string):
+    def create_vehicle(self, model_name):
+        #(2) generate a vechile id
+        vehicle_id = self.generate_id(3)
+        #(3) generate the vehicle lisence plate
+        lisence_plate = self.generate_lisence_plate(vehicle_id)
+        return Vehicle(vehicle_id, lisence_plate, self.model_info[model_name])
+
+
+class VehicleApplication:
+    def register_vehicle(self, model_name: string):
         #(1) create an instance of VechileRegistery
         registry = VehicleRegistry()
 
-        #(2) generate a vechile id
-        vehicle_id = registry.generate_id(3)
+        #create a vehicle (new)
+        vehicle = registry.create_vehicle(model_name)
 
-        #(3) generate the vehicle lisence plate
-        lisence_plate = registry.generate_lisence_plate(vehicle_id)
-
-        #(4) compute the catolgue price
-        catalogue_price = 0
-        if brand_name == "Tesla Model 3":
-            catalogue_price = 6000
-        elif brand_name == "VolksWagen ID3":
-            catalogue_price = 3500
-        elif brand_name == "BMW 5":
-            catalogue_price = 45000
-
-        #(5) compute the tax percentage, default is 0.05 except for electric cars it goes down in the tax %
-        tax_percentage = 0.05
-        if brand_name in ("Tesla Model 3", "VolksWagen ID3"):
-            tax_percentage = 0.02
-        
-        payable_tax = tax_percentage * catalogue_price
-
-        #(6) print the vechile registration info
-        print("Registeration Complete! Your vehicle Information is:")
-        print("brand: ",brand_name)
-        print("ID: ",vehicle_id)
-        print("Lisence plate: ",lisence_plate)
-        print("Price: ", catalogue_price)
-        print("Payable Tax: ",payable_tax)
+        return vehicle        
 
 
-car_app = CarApplication()
-car_app.register_vehicle("VolksWagen ID3")
+vehicle_app = VehicleApplication()
+#TODO make naming as input to the user where they first select the name
+vehicle = vehicle_app.register_vehicle("VolksWagen ID3")
+
+print("Registeration Complete! Your vehicle Information is:")
+vehicle.print()
 
